6ab0acaa11303c58a7f745ac4d366c6b2f2db6a5
Samuel Kebadu <famemansonic@gmail.com>
2014-06-03 20:37:59 -0400

    this is all files from bill from accounting

--- interface.py
+++ interface.py
@@ -1,4 +1,25 @@
 #File: interface.py
 #description: this file creates the user interface that shows items, health, etc. and updates it.
-def createInterface():
-    print "interface created"
+from Tkinter import * # <---- all imports used for main
+from multiprocessing import Process
+import ImageTk
+import ctypes
+import Image
+import sys
+import winsound
+from items import *
+from mob import *
+from interface import *
+from fileSave import *
+from Sound import *
+from Move import *
+from random import randint
+
+def createInterface(f,r,w,h,health,mhealth):
+    #var = StringVar()
+    #label = Label(root, textvariable=var, relief=RAISED)
+    bar = "health " + str(health) + "/" + str(mhealth)
+    #r.delete(f)
+    print bar
+    r.itemconfig(f, text=bar)
+    #bob.set(w,h, text = bar, fill = "red", font = ('Times', 20, 'bold'), anchor = 'nw')
