4db73be79ec07cf2e92ada28dae26e69e70b2f2f
VoidedWarranty <fiddlerdude@hotmail.com>
2015-05-11 15:34:32 -0400

    Delete monster.java

--- monster.java
+++
@@ -1,8 +0,0 @@
-public abstract class monster extends mob {
-   //need to have the stats for dmg and armor.
-   public monster(int x,int y,int direction, int health, int movespeed) {
-      super(x,y,direction,health,movespeed);
-   }
-   public abstract void attack(hero victim);
-   
-}
